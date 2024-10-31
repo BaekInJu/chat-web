537cee981921b14dc4b701aa09ae9820095bde51
dldks3467 <dldks3467@naver.com>
2024-10-31 09:19:16 +0900

    create relay-server

---
+++ relay-server/relay-server/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  relay-server
+//
+//  Created by 백인준 on 10/31/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

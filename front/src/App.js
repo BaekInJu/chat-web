cfed84e075a44e7ad476150e063483acb130a744
dldks3467 <dldks3467@naver.com>
2024-11-20 18:36:30 +0900

    add main page

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
+import MarketingPage from './marketing-page/MarketingPage';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      {/* <SignIn /> */}
+      <MarketingPage />
     </div>
   );
 }

bb0a199f426933f8ed044c46fe6fa4afa297279e
dldks3467 <dldks3467@naver.com>
2024-12-11 15:40:28 +0900

    test: 웹소켓 테스트 코드 작성

--- back-end/src/main/java/com/lhb/backend/chat/Chat.java
+++ back-end/src/main/java/com/lhb/backend/chat/Chat.java
@@ -1,5 +1,23 @@
 package com.lhb.backend.chat;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Chat {
 
+
+    @Getter
+    @Setter
+    public static class SendMessage{
+        private Long sender;
+        private String content;
+        private String time;
+    }
+
+    @Getter
+    @Setter
+    public static class ChatMessage{
+        private String time;
+        private String content;
+    }
 }
---
+++ back-end/src/main/java/com/lhb/backend/chat/ChatMessage.java
@@ -0,0 +1,11 @@
+package com.lhb.backend.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String time;
+    private String content;
+}
